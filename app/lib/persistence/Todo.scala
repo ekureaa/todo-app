3ac4f210e477df9cbd1bc56f92ce0db48faf2feb
Yuuki Ishikawa <yuuki.ishikawa@nextbeat.net>
2024-09-11 20:26:44 +0900

    TodoのRepositoryを作成

---
+++ app/lib/persistence/Todo.scala
@@ -0,0 +1,36 @@
+package lib.persistence
+
+import com.zaxxer.hikari.HikariDataSource
+
+import scala.concurrent.{ExecutionContext, Future}
+import ixias.model._
+import ixias.slick.SlickRepository
+import ixias.slick.builder.{DatabaseBuilder, HikariConfigBuilder}
+import ixias.slick.jdbc.MySQLProfile.api._
+import ixias.slick.model.DataSourceName
+import lib.model.Todo
+import lib.model.Todo.Id
+import lib.persistence.db.TodoTable
+import slick.dbio.Effect
+import slick.sql.FixedSqlAction
+
+// UserRepository: UserTableへのクエリ発行を行うRepository層の定義
+//~~~~~~~~~~~~~~~~~~~~~~
+class TodoRepository()(implicit val ec: ExecutionContext) extends SlickRepository[Todo.Id, Todo] {
+  val master: Database = DatabaseBuilder.fromHikariDataSource(
+    new HikariDataSource(HikariConfigBuilder.default(DataSourceName("ixias.db.mysql://master/to_do")).build())
+  )
+  val slave: Database = DatabaseBuilder.fromHikariDataSource(
+    new HikariDataSource(HikariConfigBuilder.default(DataSourceName("ixias.db.mysql://slave/to_do")).build())
+  )
+
+  val todoTable = TableQuery[TodoTable]
+
+  /**
+    * Get User Data
+    */
+  def getById(id: Todo.Id): Future[Option[Todo]] = {
+    slave.run(todoTable.filter(_.id === id).result.headOption)
+  }
+
+}
