0b64422773c1af1aac667cb90b5fe983821a06e2
Yuuki Ishikawa <yuuki.ishikawa@nextbeat.net>
2024-09-27 20:28:26 +0900

    TodoページでTodo全件の情報を表示する機能を追加

--- app/controllers/Todo.scala
+++ app/controllers/Todo.scala
@@ -5,12 +5,18 @@ import play.api.mvc._
 
 import model.ViewValueHome
 import lib.model.Todo
+import lib.model.Todo.Id
 import lib.persistence.TodoRepository
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.util.Success
 
 @Singleton
 class TodoController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def list() = Action { implicit req =>
+  def list() = Action.async { implicit req =>
     val vv = ViewValueHome(
       title  = "Todo一覧",
       cssSrc = Seq("main.css"),
@@ -19,6 +25,11 @@ class TodoController @Inject()(val controllerComponents: ControllerComponents) e
 
     val todoRepository = new TodoRepository()
 
-    Ok(views.html.todo.list(vv))
+    val todoFuture = todoRepository.getTodoAll()
+
+    todoFuture.map { result =>
+      Ok(views.html.todo.list(vv, result))
+    }
+
   }
 }
