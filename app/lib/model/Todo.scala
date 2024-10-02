7228108816e60ec34c9a214382988cc0821fd801
Yuuki Ishikawa <yuuki.ishikawa@nextbeat.net>
2024-10-02 20:14:37 +0900

    カテゴリとステータスの表示を改善

--- app/lib/model/Todo.scala
+++ app/lib/model/Todo.scala
@@ -11,7 +11,7 @@ case class Todo(
   id:        Option[Todo.Id],
   title:     String,
   status:    Todo.Status,
-  category:  Long,
+  category:  Todo.Category,
   content:   String,
   updatedAt: LocalDateTime = NOW,
   createdAt: LocalDateTime = NOW
@@ -30,6 +30,13 @@ object Todo {
   object Status extends EnumStatus.Of[Status] {
     case object TODO       extends Status(code = 0,   name = "TODO")
     case object INPROGRESS extends Status(code = 1, name = "進行中")
-    case object COMPRETED extends Status(code = 2, name = "完了")
+    case object COMPRETED  extends Status(code = 2, name = "完了")
+  }
+
+  sealed abstract class Category(val code: Short, val name: String) extends EnumStatus
+  object Category extends EnumStatus.Of[Category] {
+    case object FRONTEND       extends Category(code = 1, name = "フロントエンド")
+    case object BACKEND        extends Category(code = 2, name = "バックエンド")
+    case object INFRASTRUCTURE extends Category(code = 3, name = "インフラ")
   }
 }
